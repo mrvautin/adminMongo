ddf1cdad46b396c5356c1e8f15e68e9e3e0b8a3f
Mark Moffat <mrvautin@gmail.com>
2016-06-17 16:48:12 +0930

    Fixed tests

--- tests/tests.js
+++ tests/tests.js
@@ -6,26 +6,36 @@ var db_name = "NewTestDB";
 var coll_name = "NewTestCollection";
 var user_name = "TestNewUser";
 
+app = require('../app');
+var agent = request.agent(app);
+
+before(function (done) {
+    app.on("adminMongoStarted", function(){
+        done();
+    });
+});
+
 describe("Add connection, database and collection",function(){
+
     it("Add a new connection",function(done){
-        request("http://localhost:1234")
+        agent
             .post("/add_config")
-            .send({0: conn_name,1:"mongodb://localhost:27017"})
+            .send({0: conn_name,1: "mongodb://localhost:27017"})
             .expect(200)
             .expect("Config successfully added", done);
     });
     
     it("Add a database",function(done){
-        request("http://localhost:1234/" + conn_name)
-            .post("/db_create")
+        agent
+            .post("/" + conn_name + "/db_create")
             .send({"db_name" : db_name})
             .expect(200)
             .expect("Database successfully created", done);
     });
 
     it("Add a collection",function(done){
-        request("http://localhost:1234/" + conn_name)
-            .post("/" + db_name + "/coll_create")
+        agent
+            .post("/" + conn_name + "/" + db_name + "/coll_create")
             .send({"collection_name" : coll_name})
             .expect(200)
             .expect("Collection successfully created", done);
@@ -39,16 +49,16 @@ describe("User tests",function(){
             "user_password": "test",
             "roles_text": "read"
         }
-        request("http://localhost:1234/" + conn_name)
-            .post("/" + db_name + "/" + coll_name + "/user_create")
+        agent
+            .post("/" + conn_name + "/" + db_name + "/" + coll_name + "/user_create")
             .send(json)
             .expect(200)
             .expect("User successfully created", done);
     });
  
     it('Delete a user', function(done){       
-        request("http://localhost:1234/" + conn_name)
-            .post("/" + db_name + "/" + coll_name + "/user_delete")
+        agent
+            .post("/" + conn_name + "/" + db_name + "/" + coll_name + "/user_delete")
             .send({"username": user_name})
             .expect(200)
             .expect("User successfully deleted", done);
@@ -60,8 +70,8 @@ describe("Document tests",function(){
     it("Add a document",function(done){
         var json = {"NewTestDocument":"Test Data"};
         var strJSON = JSON.stringify(json);
-        request("http://localhost:1234/" + conn_name)
-            .post("/" + db_name + "/" + coll_name + "/insert_doc")
+        agent
+            .post("/" + conn_name + "/" + db_name + "/" + coll_name + "/insert_doc")
             .send({"objectData": strJSON})
             .expect(200)
             .expect("Document successfully added", done);
@@ -70,8 +80,8 @@ describe("Document tests",function(){
     it("Find our new document",function(done){
         var json = {"NewTestDocument":"Test Data"};
         var strJSON = JSON.stringify(json);
-        request("http://localhost:1234/api/" + conn_name)
-            .post("/" + db_name + "/" + coll_name + "/1")
+        agent
+            .post("/api/" + conn_name + "/" + db_name + "/" + coll_name + "/1")
             .send({"query": strJSON})
             .expect(200)
             .end(function(err, res) {
@@ -82,8 +92,8 @@ describe("Document tests",function(){
     });
  
     it('Delete our new document', function(done){       
-        request("http://localhost:1234/" + conn_name)
-            .post("/" + db_name + "/" + coll_name + "/doc_delete")
+        agent
+            .post("/" + conn_name + "/" + db_name + "/" + coll_name + "/doc_delete")
             .send({"doc_id": doc_id})
             .expect(200)
             .expect("Document successfully deleted", done);
@@ -92,31 +102,31 @@ describe("Document tests",function(){
 
 describe("Remove and remove collection and connection",function(){
     it("Rename the collection",function(done){
-        request("http://localhost:1234/" + conn_name)
-            .post("/" + db_name + "/" + coll_name + "/coll_name_edit")
+        agent
+            .post("/" + conn_name + "/" + db_name + "/" + coll_name + "/coll_name_edit")
             .send({"new_collection_name" :  coll_name + "Changed"})
             .expect(200)
             .expect("Collection successfully renamed", done);
     });
     
     it("Remove the collection",function(done){
-        request("http://localhost:1234/" + conn_name)
-            .post("/" + db_name + "/coll_delete")
+        agent
+            .post("/" + conn_name + "/" + db_name + "/coll_delete")
             .send({"collection_name" : coll_name + "Changed"})
             .expect(200)
             .expect("Collection successfully deleted", done);
     });
     
     it("Remove the database",function(done){
-        request("http://localhost:1234/" + conn_name)
-            .post("/db_delete")
+        agent
+            .post("/" + conn_name + "/db_delete")
             .send({"db_name" : db_name})
             .expect(200)
             .expect("Database successfully deleted", done);
     });
 
     it("Remove the connection",function(done){
-        request("http://localhost:1234")
+        agent
             .post("/drop_config")
             .send({"curr_config": conn_name})
             .expect(200)
