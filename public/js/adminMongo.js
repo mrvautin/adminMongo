61c3d35580907e6d76f08d1f1cc96793af181851
ChenNengNeng <38547122+ChenNengNeng@users.noreply.github.com>
2018-08-08 18:18:24 +0800

    Update public/js/adminMongo.js
    fix the value of an integer string cannot be queried.

--- public/js/adminMongo.js
+++ public/js/adminMongo.js
@@ -110,7 +110,7 @@ $(document).ready(function(){
                 val = parseInt(val);
             }else{
             // if we find an integer wrapped in quotes
-                var strIntReg = new RegExp('^"[0-9]"+$');
+                var strIntReg = new RegExp('^"[0-9]+"$');
                 if(val.match(strIntReg)){
                     val = val.replace(/"/g, '');
                 }
