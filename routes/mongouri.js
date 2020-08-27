d1aac049e7337104c6bce4cfef1c9277249d680c
Andreas Berger <berger.accso@ard.de>
2020-08-28 00:36:38 +0200

    + more debug due to missing db_name

--- routes/mongouri.js
+++ routes/mongouri.js
@@ -5,13 +5,15 @@ const RE_PULL_DATABASE = /^\/([^?]*)($|\?.*$)/;
 const RE_PULL_OPTIONS = /^\?(.*)$/;
 
 exports.parse = function (uri){
+    console.log('[DEBUG, mongouri.js, .parse] uri: ' + uri);
     let uri_trimmed = uri.trim();
     let scheme = MongoUri.pullMongodbScheme(uri_trimmed);
     let auth = MongoUri.pullAuth(scheme[2]);
     let hosts = MongoUri.pullHosts(auth[3]);
     let database = MongoUri.pullDatabase(hosts[2]);
     let options = MongoUri.pullOptions(database[2]);
-
+    let mongoUri = new MongoUri(scheme[1], decodeURIComponent(auth[1]), decodeURIComponent(auth[2]), hosts[1], database[1], options);
+    console.log('[DEBUG, mongouri.js, .parse] MongoUri: ' + mongoUri);
     return new MongoUri(scheme[1], decodeURIComponent(auth[1]), decodeURIComponent(auth[2]), hosts[1], database[1], options);
 };
 
